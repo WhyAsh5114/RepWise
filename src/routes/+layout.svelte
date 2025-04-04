15827c430b08e32cbf73c7481360295a9f10241e
Jay Kerkar <bytewise0405@gmail.com>
2025-04-04 17:19:47 +0000

    feat: implement authentication check for protected routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
 	import '../app.css';
+	import { page } from '$app/state';
+	import { goto } from '$app/navigation';
+
 	let { children } = $props();
+
+	const unprotectedRoutes = ['/', '/login'];
+	let isAuthenticated: Boolean = false;
+
+	$effect(() => {
+		if (!isAuthenticated && !unprotectedRoutes.includes(page.url.pathname)) {
+			goto('/login');
+		}
+	});
 </script>
 
 {@render children()}
